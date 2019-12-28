a77ddcb3b0d6a96b4ddd0fe5629de3b2a531fb5e
David Nguyen <hpcsc@users.noreply.github.com>
2019-12-29 08:47:07 +1100

    add error checking

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.11.0
 
-RUN apk --no-cache add curl
+RUN apk --no-cache add curl jq
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,33 @@ PACKAGE=$2
 VERSION=$3
 SOURCE_PATH=$4
 
+UPLOADED=0
 for FILE in ${SOURCE_PATH}; do
     UPLOAD_URL=https://api.bintray.com/content/${INPUT_USERNAME}/${REPOSITORY}/$(basename $FILE)
-    echo "=== Uploading ${FILE} to ${URL}"
-    curl -T ${FILE} \
+    echo "=== Uploading ${FILE} to ${UPLOAD_URL}"
+    RESULT=$(curl -T ${FILE} \
             -u${INPUT_USERNAME}:${INPUT_APIKEY} \
             -H "X-Bintray-Package:${PACKAGE}" \
             -H "X-Bintray-Version:${VERSION}" \
-            ${UPLOAD_URL}
+            ${UPLOAD_URL})
+
+    if [ "$(echo ${RESULT} | jq -r '.message')" != "success" ]; then
+        echo "=== Failed to upload ${FILE} with response: ${RESULT}"
+        exit 1
+    else
+        UPLOADED=$((UPLOADED+1))
+        echo "=== ${FILE} uploaded successfully"
+    fi
 done
 
 echo "=== Publishing repository [${REPOSITORY}] - package [${PACKAGE}] - version [${VERSION}]"
-curl -X POST \
+PUBLISH_RESULT=$(curl -X POST \
         -u${INPUT_USERNAME}:${INPUT_APIKEY} \
-        https://api.bintray.com/content/${INPUT_USERNAME}/${REPOSITORY}/${PACKAGE}/${VERSION}/publish
+        https://api.bintray.com/content/${INPUT_USERNAME}/${REPOSITORY}/${PACKAGE}/${VERSION}/publish)
+
+if [ $(echo ${PUBLISH_RESULT} | jq -r '.files') -ne ${UPLOADED} ]; then
+    echo "=== Not all files are published successfully, response: ${PUBLISH_RESULT}"
+    exit 1
+else
+    echo "=== Published successfully"
+fi
