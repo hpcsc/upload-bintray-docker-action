d6cfe500164a5e7536d59a73a0a63ffa705d90cb
David Nguyen <hpcsc@users.noreply.github.com>
2020-11-14 14:49:18 +1100

    Use bintray api to check for publish result

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ UPLOADED=0
 for FILE in ${SOURCE_PATH}; do
     UPLOAD_URL=https://api.bintray.com/content/${INPUT_USERNAME}/${REPOSITORY}/$(basename $FILE)
     echo "=== Uploading ${FILE} to ${UPLOAD_URL}"
-    RESULT=$(curl -T ${FILE} \
+    RESULT=$(curl -T "${FILE}" \
             -u${INPUT_USERNAME}:${INPUT_APIKEY} \
             -H "X-Bintray-Package:${PACKAGE}" \
             -H "X-Bintray-Version:${VERSION}" \
@@ -26,13 +26,35 @@ for FILE in ${SOURCE_PATH}; do
 done
 
 echo "=== Publishing repository [${REPOSITORY}] - package [${PACKAGE}] - version [${VERSION}]"
-PUBLISH_RESULT=$(curl -X POST \
+curl -X POST \
         -u${INPUT_USERNAME}:${INPUT_APIKEY} \
-        https://api.bintray.com/content/${INPUT_USERNAME}/${REPOSITORY}/${PACKAGE}/${VERSION}/publish)
+        https://api.bintray.com/content/${INPUT_USERNAME}/${REPOSITORY}/${PACKAGE}/${VERSION}/publish
 
-if [ $(echo ${PUBLISH_RESULT} | jq -r '.files') -ne ${UPLOADED} ]; then
-    echo "=== Not all files are published successfully, response: ${PUBLISH_RESULT}"
-    exit 1
-else
-    echo "=== Published successfully"
-fi
+TIMEOUT=10
+for i in $(seq ${TIMEOUT}); do
+    echo "\n=== Checking result of publish, attempt: ${i}"
+    PUBLISHED_FILES=$(curl -s -u${INPUT_USERNAME}:${INPUT_APIKEY} \
+        https://api.bintray.com/packages/${INPUT_USERNAME}/${REPOSITORY}/${PACKAGE}/versions/${VERSION}/files)
+    ALL_PUBLISHED=1
+    for FILE in ${SOURCE_PATH}; do
+        FILE_NAME=$(basename ${FILE})
+        PUBLISHED=$(echo ${PUBLISHED_FILES} | jq -r '.[].name | select(. == "'${FILE_NAME}'")')
+        if [ -z "${PUBLISHED}" ]; then
+            ALL_PUBLISHED=0
+            echo "${FILE_NAME} not yet published"
+        else
+            echo "${FILE_NAME} published"
+        fi
+    done
+
+    if [ "${ALL_PUBLISHED}" = "0" ]; then
+        echo "Not all files are published, sleeping for 2s"
+        sleep 2s
+    else
+        echo "All files published successfully"
+        exit 0
+    fi
+done
+
+echo "=== Not all files are published after ${TIMEOUT} checks"
+exit 1
